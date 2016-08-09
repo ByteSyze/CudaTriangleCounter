fdc7aa38f471b84bfcc38ee8c7ff28fad8d904b9
Insomniac10102 <ins10102@gmail.com>
2016-08-09 02:18:30 -0700

    Initial main file

---
+++ CUDA Triangle Counter/src/main.cu
@@ -0,0 +1,22 @@
+/**
+*
+* Copyright (C) Tyler Hackett 2016
+*
+* CUDA Triangle Counter
+*
+* A quickly-written program to determine all possible combinations of
+* valid triangles from a grid, allowing for certain coordinates of the
+* grid to be marked as unusable.
+*
+* main.cu
+*
+* */
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
